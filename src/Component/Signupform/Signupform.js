a72b3d5ea8dcac91c4646142ba777a94ec9e2534
Joydeep Maiti <joydeep127@gmail.com>
2018-08-04 17:45:12 +0530

    Basic routing done

---
+++ src/Component/Signupform/Signupform.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import classes from './Signupform.css';
+
+const signupform = () => {
+    return (
+        <div className={classes.form}>
+            <div className={classes.formsignup}>
+                <form>
+                    <lable>Full Name</lable>
+                        <input type="text" placeholder="Your full name" />
+                    <lable>Email</lable>
+                        <input type="email" placeholder="Your e-mail" />
+                    <lable>Username</lable>
+                        <input type="text" placeholder="Your username" />
+                    <lable>Password</lable>
+                        <input type="password"placeholder="Your password" />
+                    <lable>Retype Password</lable>
+                        <input type="password"placeholder="Your password" />
+                    <p class="terms">
+                        <input type="checkbox" /> I agree all statments in terms of service
+                    </p>
+                        <input type="submit" className={classes.formbtn} value="Sign Up" />
+                        <a href="#" className={classes.linedlink}>Signin -></a>
+
+                </form>
+            </div>                   
+        </div>
+    )
+}
+
+export default signupform;
--- src/Container/Signup/Signup.js
+++ src/Container/Signup/Signup.js
@@ -1,11 +1,13 @@
 import React, { Component } from "react";
 import classes from './Signup.css';
+import Signupform from '../../Component/Signupform/Signupform';
 
 class Signup extends Component {
     render() {
         return (
             <div className={classes.signuppage}>
                 <div className={classes.image}></div>
+                <Signupform />
             </div>
         )
     }
