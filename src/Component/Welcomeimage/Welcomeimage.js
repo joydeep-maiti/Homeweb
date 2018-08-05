e42c535903e9e495e39b7d91148b808ec36de537
Joydeep Maiti <joydeep127@gmail.com>
2018-08-06 00:18:24 +0530

    Adding responsive styling

--- src/Component/Welcomeimage/Welcomeimage.js
+++ src/Component/Welcomeimage/Welcomeimage.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import classes from './Welcomeimage.css';
+import icon from '../../assets/search.png';
 
 const Welcomeimage = () => {
     return (
         <div className={classes.slider}>
             <div className={classes.area}>
-                <span className={classes.caption}>Book unique Event with one</span>
-                <span className={classes.caption}>stop store and experience event memories .</span>
-                <input type="text" className={classes.inputfield} placeholder="search wedding or Photographer in your city"/>
+                <span className={classes.caption}>Book unique Event with one stop store and experience event memories.</span>
+                <div className={classes.input}>
+                    <input className={classes.inputfield} type="text"  placeholder="search wedding or Photographer in your city" />
+                    <img src={icon} />
+                </div>
+                
             </div>
             <p className={classes.footerbtn}>footer</p>
         </div>
