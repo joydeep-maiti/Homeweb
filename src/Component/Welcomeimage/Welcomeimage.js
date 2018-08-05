486cee94bd27b4fe01f7ade23ef44e3269f670b9
Joydeep Maiti <joydeep127@gmail.com>
2018-08-06 01:10:04 +0530

    Modifyng footer btn

--- src/Component/Welcomeimage/Welcomeimage.js
+++ src/Component/Welcomeimage/Welcomeimage.js
@@ -4,16 +4,16 @@ import icon from '../../assets/search.png';
 
 const Welcomeimage = () => {
     return (
-        <div className={classes.slider}>
+        <div className={classes.slider} id="change">
             <div className={classes.area}>
                 <span className={classes.caption}>Book unique Event with one stop store and experience event memories.</span>
                 <div className={classes.input}>
                     <input className={classes.inputfield} type="text"  placeholder="search wedding or Photographer in your city" />
-                    <img src={icon} />
+                    <img src={icon} alt="search icon"/>
                 </div>
                 
             </div>
-            <p className={classes.footerbtn}>footer</p>
+            <p className={classes.footerbtn}>Terms,Privacy,Currency & More</p>
         </div>
     );
 }
